66d367bca88fc5f08b1e0839e5b95096cf65a336
joaopugsley <canal.insannity@gmail.com>
2023-09-16 16:58:58 -0300

    fixing navbar

--- src/components/Navbar/NavItem.tsx
+++ src/components/Navbar/NavItem.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { usePathname } from "next/navigation";
-import Link from "next/link";
 
 export type NavItemProps = {
   href: string
@@ -13,13 +12,13 @@ const NavItem = ({ href, title, className }: NavItemProps): JSX.Element => {
   const pathname = usePathname()
   const active = href === pathname;
   return (
-    <Link href={href}>
+    <a href={href}>
       <span className={`
         ${className} ${!!active ? "bg-gradient-to-l from-violet-500 to-blue-700 rounded-lg" : "text-white ease-in-out transition-all duration-150 bg-clip-text bg-gradient-to-r hover:text-transparent from-violet-500 to-blue-700"}
       `}>
         {title}
       </span>
-    </Link>
+    </a>
   )
 }
 
